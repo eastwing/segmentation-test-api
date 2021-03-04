0f125edfb126f3dd064fb1bc7e5cb1049f9fff0a
justpsst <justpsst@yandex.ru>
2021-03-04 14:49:37 +0500

    fixes

--- src/main.ts
+++ src/main.ts
@@ -2,21 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 
-
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule);
 
-//     const microservice = app.connectMicroservice(
-//     {
-//         transport: Transport.RMQ,
-//         options: {
-//             urls: [`amqp://localhost:5673`],
-//             queue: 'my_queue',
-//             queueOptions: { durable: false },
-//         }
-//     });
+  //     const microservice = app.connectMicroservice(
+  //     {
+  //         transport: Transport.RMQ,
+  //         options: {
+  //             urls: [`amqp://localhost:5673`],
+  //             queue: 'my_queue',
+  //             queueOptions: { durable: false },
+  //         }
+  //     });
 
-//   await app.startAllMicroservicesAsync();
-  await app.listen(3000);
+  //   await app.startAllMicroservicesAsync();
+  await app.listen(3000, () => console.log('API started'));
 }
 bootstrap();
